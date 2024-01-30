bugs (overflow + tuned sigma) fixed

distributions.py
@@ -9,7 +9,7 @@ class Distribution(ABC):
     """Abstract class for distribution"""
 
     @abstractmethod
-    def log_prob(self, z: torch.Tensor) -> torch.Tensor:
+    def log_prob(self, z: Tensor) -> Tensor:
         """Computes log probability of input z"""
         raise NotImplementedError
 
@@ -22,13 +22,14 @@ class SamplableDistribution(Distribution):
 
 
 class GaussianMixture(SamplableDistribution):
-    def __init__(self, means, covs, weights):
-        self.weights = Tensor(weights)
+    def __init__(self, means, covs, weights: Tensor):
+        self.weights = weights
         self.category = torch.distributions.Categorical(self.weights)
         self.means = means
         self.covs = covs
 
     def sample(self, sample_count: int) -> Tensor:
+        # which_gaussian = self.category.sample(torch.Size((sample_count,)))
         which_gaussian = self.category.sample(torch.Size((sample_count,)))
         multivariate_normal = torch.distributions.MultivariateNormal(
             self.means[which_gaussian], self.covs[which_gaussian])
@@ -36,5 +37,8 @@ class GaussianMixture(SamplableDistribution):
         return Tensor(multivariate_normal.sample())
     
     def log_prob(self, z: Tensor) -> Tensor:
-        logs = torch.distributions.MultivariateNormal(self.means, self.covs).log_prob(z[:, None, :])
-        return Tensor(torch.log(torch.exp(logs) @ self.weights))
+        logs = torch.distributions.MultivariateNormal(
+            self.means, self.covs).log_prob(z[:, None, :])
+        logs += torch.log(self.weights)
+
+        return Tensor(torch.logsumexp(logs, dim=1))

samplers/fisher_mala.py
@@ -81,11 +81,11 @@ def fisher_mala(
         point = point.detach().requires_grad_()
 
     R = torch.eye(point.shape[-1]).repeat(*point.shape[:-1], 1, 1)
-    sigma_R = meta["sigma"][-1][..., None]
+    sigma_R = meta["sigma"][..., None]
     # print("sigma_R", sigma_R)
 
-    grad_x = meta["grad"][-1]
-    logp_x = meta["logp"][-1]
+    grad_x = meta["grad"]
+    logp_x = meta["logp"]
 
     sigma_ = sigma_R.clone()
 
@@ -94,6 +94,10 @@ def fisher_mala(
     
     pbar = tqdm.trange if verbose else range
     for step_id in pbar(sample_count + burn_in_prec):
+
+        h_ = partial(h, prec_factors=[R, R.permute(0, 2, 1)], keep_graph=keep_graph,
+                     target_dist=target_dist, sigma=sigma_R.squeeze())
+
         # print("step", step_id)
         noise = proposal_dist.sample(point.shape[:-1])
 
@@ -126,6 +130,7 @@ def fisher_mala(
             0
         ]  # .detach()
 
+        # Inefficient O(d^3) calculation of the inverse 
         # grad_y_img = grad_y[..., None]
         # grad_y_img = R @ (R.permute(0, 2, 1) @ grad_y_img)
 
@@ -136,33 +141,43 @@ def fisher_mala(
         #         (point - proposal_point - (0.5 * grad_y_img * sigma_R ** 2).squeeze())[..., None]
         #     ).squeeze()
         # )
-
+        #
         # accept_prob = torch.clamp((logp_y + log_qxy - logp_x - log_qyx).exp(), max=1)
         accept_prob = torch.clamp(
             torch.exp(
-                logp_y + h_(point, proposal_point, sigma_R.squeeze()) - logp_x \
-                - h_(proposal_point, point, sigma_R.squeeze())
+                logp_y + h_(point, proposal_point) - logp_x - h_(proposal_point, point)
             ),
             max=1
         )
+
+        if not keep_graph:
+            accept_prob = accept_prob.detach()
         # print("accept_prob", accept_prob)
         # print("nan accept_prob", torch.isnan(accept_prob).sum())
 
 
         # print("accept", accept_prob.shape)
         # print("grad_y - grad_x", (grad_y - grad_x).shape)
-
+        
         signal_adaptation = torch.sqrt(accept_prob)[..., None] * (grad_y - grad_x)
+
+        #if not keep_graph: 
+        #    signal_adaptation = signal_adaptation.detach()
         # print("nan signal_adaptation", torch.isnan(signal_adaptation).sum())
         # print("sqrt accept", torch.sqrt(accept_prob))
         
         # print("sig adapt", signal_adaptation)
 
         phi_n = R.permute(0, 2, 1) @ signal_adaptation[..., None]
+        #if not keep_graph: 
+        #    phi_n = phi_n.detach()
         # print("nan phi_n", torch.isnan(phi_n).sum())
         # print("phi_n", phi_n)
 
         gramm_diag = phi_n.permute(0, 2, 1) @ phi_n
+        #if not keep_graph:
+        #    gramm_diag = gramm_diag.detach()
+
         # print("gramm_diag", gramm_diag)
 
         if step_id == 0:
@@ -227,7 +242,7 @@ def fisher_mala(
                 grad_x[mask] = grad_y[mask]
 
         meta["mh_accept"].append(accept_prob)
-        meta["sigma"].append(sigma_R)
+        meta["sigma"] = sigma_R
 
         if not keep_graph:
             point = point.detach().requires_grad_()
@@ -237,7 +252,7 @@ def fisher_mala(
         
     chains = torch.stack(chains, 0)
 
-    meta["logp"].append(logp_x.detach())
-    meta["grad"].append(grad_x.detach())
+    meta["logp"] = logp_x.detach()
+    meta["grad"] = grad_x.detach()
 
     return chains, meta

samplers/mala_ex2mcmc.py
@@ -86,11 +86,12 @@ def mala(
         noise = proposal_dist.sample(point.shape[:-1])
         proposal_point = point + step_size * grad_x + noise * (2 * step_size) ** 0.5
         proposal_point = project(proposal_point)
+
         if not keep_graph:
             proposal_point = proposal_point.detach().requires_grad_()
 
         logp_y = target_dist.log_prob(proposal_point)
-        # print("logp_y", logp_y)
+
         grad_y = torch.autograd.grad(
             logp_y.sum(),
             proposal_point,
@@ -104,6 +105,14 @@ def mala(
         log_qxy = proposal_dist.log_prob(
             (point - proposal_point - step_size * grad_y) / (2 * step_size) ** 0.5
         )
+
+        # print("logpy", logp_y)
+        # print("logpx", logp_x)
+
+        # print("logqxy", log_qxy)
+        # print("logqyx", log_qyx)
+
+        # print("logaccept", logp_y + log_qxy - logp_x - log_qyx)
 
         accept_prob = torch.clamp((logp_y + log_qxy - logp_x - log_qyx).exp(), max=1)
         mask = torch.rand_like(accept_prob) < accept_prob

samplers/mala_modified.py
@@ -46,23 +46,22 @@ def mala(
     meta = meta or dict()
     meta["mh_accept"] = meta.get("mh_accept", [])
     
-    meta["logp"] = meta.get("logp", [])
-    meta["logp"].append(logp_x)
-    meta["sigma"] = meta.get("sigma", [])
+    meta["logp"] = meta.get("logp", logp_x)
+    meta["sigma"] = meta.get("sigma", sigma)
 
     if "grad" not in meta:
         if keep_graph:
             grad_x = torch.autograd.grad(
-                meta["logp"][-1].sum(),
+                meta["logp"].sum(),
                 point,
                 create_graph=keep_graph,
                 retain_graph=keep_graph,
             )[0]
         else:
             grad_x = torch.autograd.grad(logp_x.sum(), point)[0].detach()
-        meta["grad"] = [grad_x]
+        meta["grad"] = grad_x
     else:
-        grad_x = meta["grad"][-1]
+        grad_x = meta["grad"]
 
     pbar = tqdm.trange if verbose else range
 
@@ -104,10 +103,7 @@ def mala(
         # print("log_qxy", log_qxy.shape)
         
         accept_prob = torch.clamp((logp_y + log_qxy - logp_x - log_qyx).exp(), max=1)
-        # print('accept', accept_prob.shape)
-
         mask = torch.rand_like(accept_prob) < accept_prob
-        mask = mask.detach()
 
         if keep_graph:
             mask_f = mask.float()[..., None]
@@ -119,19 +115,24 @@ def mala(
                 point[mask] = proposal_point[mask]
                 logp_x[mask] = logp_y[mask]
                 grad_x[mask] = grad_y[mask]
-                # point = point * (1 - mask_f) + proposal_point * mask_f
-                # logp_x = logp_x * (1 - mask_f).squeeze() + logp_y * mask_f.squeeze()
-                # grad_x = grad_x * (1 - mask_f) + grad_y * mask_f
 
         # print("point", point.shape)
         # print("logpx", logp_x.shape)
         # print("grad_x", grad_x.shape)
         meta["mh_accept"].append(accept_prob.detach())
         # print('accept', accept_prob[..., None].shape)
+        
+        if not keep_graph:
+            accept_prob = accept_prob.detach()
+
         sigma *= (1 + rho * (accept_prob[..., None] - alpha)) ** 0.5
+
+        if not keep_graph:
+            sigma = sigma.detach()
+
         # print("sigma", sigma.shape)
         
-        meta["sigma"].append(sigma.detach())
+        meta["sigma"] = sigma.detach().cpu().clone()
 
         if not keep_graph:
             point = point.detach().requires_grad_()
@@ -142,7 +143,7 @@ def mala(
         # print()
     chains = torch.stack(chains, 0)
 
-    meta["logp"].append(logp_x)
-    meta["grad"].append(grad_x)
+    meta["logp"] = logp_x
+    meta["grad"] = grad_x
 
     return chains, meta